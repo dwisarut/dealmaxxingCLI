635f2bb039f84acf639712834a6409d7b45ad0a5
d_wisarut <d.wisarut@hotmail.com>
2026-03-05 16:56:03 +0700

    feat: add cmd flag

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,44 @@
-package root
+package cmd
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-func InitializeCLI() {
+func InitCLI() {
+	var search string
+	var query string
+	var command bool
+
+	flag.StringVar(&search, "search", "", "Search game title")
+	flag.StringVar(&query, "query", "", "Query deal")
+	flag.BoolVar(&command, "cmd", false, "Show commands")
+
+	initMessage()
+
+	flag.Parse()
+
+	if command {
+		showCommand()
+	}
+
+	// if query != "" {
+	// 	handleQuery(query)
+	// }
+
+	// if search != "" {
+	// 	handleSearch(search)
+	// }
+}
+
+func showCommand() {
+	fmt.Println("Here's an available commands:")
+	fmt.Println("-search					Searching for a specific game")
+	fmt.Println("-query					Query lists of game with specific parameter")
+}
+
+func initMessage() {
 	fmt.Println("DealmaxxingCLI")
+	fmt.Println("Insert your command here.")
+	fmt.Println("Type -cmd to find an available commands!")
 }
