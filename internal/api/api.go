215df9b7410457fa44516054aa3bba280a8ff37c
d_wisarut <d.wisarut@hotmail.com>
2026-03-10 18:20:36 +0700

    feat: add working GetDealID

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,13 +1,16 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"time"
+
+	"github.com/dwisarut/dealmaxxingCLI/internal/model"
 )
 
-func SearchGameId(id string) {
+func searchGameFromId(id string) {
 	url := "https://www.cheapshark.com/api/1.0/deals?id=" + id
 	method := "GET"
 
@@ -29,13 +32,35 @@ func SearchGameId(id string) {
 	fmt.Println(string(body))
 }
 
+func GetDealId(name string) []model.SearchGameID {
+	url := "https://www.cheapshark.com/api/1.0/games?title=" + name
+	method := "GET"
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	req, err := http.NewRequest(method, url, nil)
+
+	errorHandler(err)
+	res, err := client.Do(req)
+
+	errorHandler(err)
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+	errorHandler(err)
+
+	var lists []model.SearchGameID
+	err = json.Unmarshal(body, &lists)
+	errorHandler(err)
+
+	return lists
+}
+
 func errorHandler(err error) {
 	if err != nil {
 		fmt.Println(err)
-		return
+		panic(err)
 	}
 }
-
-// func getDealId(name string) {
-
-// }
