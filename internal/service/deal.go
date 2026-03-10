eeb9d06f1ebbf64d0775700fa12d846e3d172246
d_wisarut <d.wisarut@hotmail.com>
2026-03-10 18:40:02 +0700

    feat: add MakeRedirectLink for stores links

--- internal/service/deal.go
+++ internal/service/deal.go
@@ -1,1 +1,10 @@
-package deal
+package service
+
+import "github.com/dwisarut/dealmaxxingCLI/internal/model"
+
+func MakeRedirectLink(lists []model.SearchGameID) []model.SearchGameID {
+	for i := range lists {
+		lists[i].Redirect = "https://www.cheapshark.com/redirect?dealID=" + lists[i].DealID
+	}
+	return lists
+}
