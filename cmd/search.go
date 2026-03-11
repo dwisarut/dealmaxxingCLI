74c42b2e4fc88cdc0c84b153ff2555897fad6a6b
d_wisarut <d.wisarut@hotmail.com>
2026-03-12 00:05:55 +0700

    feat: add color to cli

--- cmd/search.go
+++ cmd/search.go
@@ -8,6 +8,7 @@ import (
 	"github.com/dwisarut/dealmaxxingCLI/internal/api"
 	"github.com/dwisarut/dealmaxxingCLI/internal/model"
 	"github.com/dwisarut/dealmaxxingCLI/internal/service"
+	"github.com/fatih/color"
 )
 
 func SearchHandler(reader *bufio.Reader, input string) {
@@ -22,32 +23,36 @@ func SearchHandler(reader *bufio.Reader, input string) {
 		var lists []model.SearchGameID = api.GetDealFromTitle(title, pageNum, 5)
 		var displayLists []model.SearchGameID = service.MakeRedirectLink(lists)
 
-		fmt.Println("Page", pageNum)
+		fmt.Println(color.HiMagentaString("Page:"), pageNum)
 		fmt.Println()
 		for _, list := range displayLists {
-			fmt.Println("Title:", list.Title)
+			fmt.Println(color.HiCyanString(list.Title))
 			fmt.Println("ID:", list.GameIDTag)
 			fmt.Println("Prices:", list.SalePrice, "$")
-			fmt.Println("Link:", list.Redirect)
+			fmt.Println(color.GreenString("Link:"), color.GreenString(list.Redirect))
 			fmt.Println()
 		}
-		fmt.Println("Commands: prev | next | cc (cancel)")
+		fmt.Println("Commands:", color.HiYellowString("prev (p)"), "|", color.HiGreenString("next (n)"), "|", color.HiRedString("cancel (cc)"))
 		fmt.Print("> ")
 		cmd, _ := reader.ReadString('\n')
 		cmd = strings.TrimSpace(cmd)
 
 		switch cmd {
-		case "next":
-			pageNum++
+		case "n":
+			if pageNum < len(lists) {
+				pageNum++
+			}
 			fmt.Println()
 
-		case "prev":
+		case "p":
 			if pageNum > 1 {
 				pageNum--
 			}
 			fmt.Println()
 
 		case "cc":
+			println("Exiting search function!")
+			fmt.Println()
 			return
 
 		default:
