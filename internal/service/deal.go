f3d40786532792a93aa018a3a130e1322fc34b42
d_wisarut <d.wisarut@hotmail.com>
2026-03-13 19:15:42 +0700

    feat: add IndexingStore, MakeRedirect, and MatchGetStore helper function

--- internal/service/deal.go
+++ internal/service/deal.go
@@ -4,20 +4,14 @@ import (
 	"github.com/dwisarut/dealmaxxingCLI/internal/model"
 )
 
-func MakeRedirectLink(lists []model.SearchGameID) []model.SearchGameID {
+func MakeSearchRedirect(lists []model.SearchGameID) []model.SearchGameID {
 	for i := range lists {
 		lists[i].Redirect = "https://www.cheapshark.com/redirect?dealID=" + lists[i].DealID
 	}
 	return lists
 }
 
-func MatchStore(lists []model.SearchGameID, stores []model.StoreLists) []model.SearchGameID {
-	storeLookup := make(map[string]string) // Hashmap
-
-	for _, store := range stores {
-		storeLookup[store.ID] = store.Name
-	}
-
+func MatchSearchStore(lists []model.SearchGameID, storeLookup map[string]string) []model.SearchGameID {
 	for i := range lists {
 		if val, ok := storeLookup[lists[i].StoreID]; ok { // JS Map.has() & Map.get() equivalent
 			lists[i].StoreName = val
@@ -25,4 +19,33 @@ func MatchStore(lists []model.SearchGameID, stores []model.StoreLists) []model.S
 	}
 
 	return lists
+}
+
+func IndexingStore(stores []model.StoreLists) map[string]string {
+	var storeLookup map[string]string
+	storeLookup = make(map[string]string)
+
+	for _, store := range stores {
+		storeLookup[store.ID] = store.Name
+	}
+
+	return storeLookup
+}
+
+func MakeGetRedirect(list model.GetGameID) model.GetGameID {
+	for i := range list.Deals {
+		list.Deals[i].Redirect = "https://www.cheapshark.com/redirect?dealID=" + list.Deals[i].DealID
+	}
+
+	return list
+}
+
+func MatchGetStore(list model.GetGameID, storeLookup map[string]string) model.GetGameID {
+	for i := range list.Deals {
+		if val, ok := storeLookup[list.Deals[i].StoreID]; ok { // JS Map.has() & Map.get() equivalent
+			list.Deals[i].StoreName = val
+		}
+	}
+
+	return list
 }
