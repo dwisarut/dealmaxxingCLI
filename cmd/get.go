2b9cd98e48721f9f162cb4695f8eacea7fe25ae8
d_wisarut <d.wisarut@hotmail.com>
2026-03-13 19:16:16 +0700

    feat: add GetHandler function

---
+++ cmd/get.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+
+	"github.com/dwisarut/dealmaxxingCLI/internal/api"
+	"github.com/dwisarut/dealmaxxingCLI/internal/model"
+	"github.com/dwisarut/dealmaxxingCLI/internal/service"
+	"github.com/fatih/color"
+)
+
+func GetHandler(reader *bufio.Reader, input string, storeIndex map[string]string) {
+	var id string = CommonParser(input)
+
+	fmt.Println()
+	color.Green("Getting...")
+	fmt.Println()
+
+	var game model.GetGameID = api.GetGameFromId(id)
+
+	if len(game.Deals) == 0 {
+		fmt.Println(color.HiRedString("No deal currently available."))
+		fmt.Println("Exiting...")
+		fmt.Println()
+		return
+	}
+
+	var displayList model.GetGameID = service.MakeGetRedirect(game)
+	displayList = service.MatchGetStore(displayList, storeIndex)
+
+	color.HiWhite("Currently available deals:")
+	fmt.Println()
+	for _, list := range displayList.Deals {
+		fmt.Println(color.HiCyanString(displayList.Info.Title))
+		fmt.Println(color.HiBlueString("Store:"), color.BlueString(list.StoreName))
+		fmt.Println(color.HiYellowString("Prices:"), color.YellowString(list.Price), color.YellowString("$"))
+		fmt.Println(color.HiWhiteString("Link:"), color.GreenString(list.Redirect))
+		fmt.Println()
+	}
+}
