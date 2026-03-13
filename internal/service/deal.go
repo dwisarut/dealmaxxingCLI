4927243fd92f1554d60ff5264ba736266e38a3df
d_wisarut <d.wisarut@hotmail.com>
2026-03-13 15:02:08 +0700

    feat: add match store function

--- internal/service/deal.go
+++ internal/service/deal.go
@@ -1,10 +1,28 @@
 package service
 
-import "github.com/dwisarut/dealmaxxingCLI/internal/model"
+import (
+	"github.com/dwisarut/dealmaxxingCLI/internal/model"
+)
 
 func MakeRedirectLink(lists []model.SearchGameID) []model.SearchGameID {
 	for i := range lists {
 		lists[i].Redirect = "https://www.cheapshark.com/redirect?dealID=" + lists[i].DealID
 	}
+	return lists
+}
+
+func MatchStore(lists []model.SearchGameID, stores []model.StoreLists) []model.SearchGameID {
+	storeLookup := make(map[string]string) // Hashmap
+
+	for _, store := range stores {
+		storeLookup[store.ID] = store.Name
+	}
+
+	for i := range lists {
+		if val, ok := storeLookup[lists[i].StoreID]; ok { // JS Map.has() & Map.get() equivalent
+			lists[i].StoreName = val
+		}
+	}
+
 	return lists
 }
