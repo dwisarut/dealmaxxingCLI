c48c9eeb12966c5141429367f21d6d15729e3f1f
d_wisarut <d.wisarut@hotmail.com>
2026-04-10 16:21:53 +0700

    feat: add cloudflare KV

---
+++ internal/api/cloudflare.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/cloudflare/cloudflare-go/v6"
+	"github.com/cloudflare/cloudflare-go/v6/kv"
+	"github.com/cloudflare/cloudflare-go/v6/option"
+	"github.com/dwisarut/dealmaxxingCLI/internal/model"
+	"github.com/joho/godotenv"
+)
+
+func ReadEnv() model.Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file!")
+	}
+
+	apiKey := os.Getenv("CLOUDFLARE_API_TOKEN")
+	namespaceID := os.Getenv("CLOUDFLARE_NAMESPACE_ID")
+	accountID := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
+
+	config := model.Config{
+		APIToken:    apiKey,
+		NamespaceID: namespaceID,
+		AccountID:   accountID,
+	}
+
+	return config
+}
+
+func NewService(env model.Config) *cloudflare.Client {
+	client := cloudflare.NewClient(
+		option.WithAPIToken(env.APIToken),
+	)
+
+	return client
+}
+
+func GetKV(config model.Config, client *cloudflare.Client) model.ProcessedKV {
+
+	page, err := client.KV.Namespaces.Keys.List(
+		context.TODO(),
+		config.NamespaceID,
+		kv.NamespaceKeyListParams{
+			AccountID: cloudflare.F(config.AccountID),
+		},
+	)
+
+	if err != nil {
+		log.Fatal("KV list failed!")
+	}
+
+	var data model.ProcessedKV
+
+	for _, item := range page.Result {
+		processed := model.Keys{
+			Name: item.Name,
+		}
+		data.Result = append(data.Result, processed)
+	}
+
+	data.Count = int(page.ResultInfo.Count)
+	return data
+}
